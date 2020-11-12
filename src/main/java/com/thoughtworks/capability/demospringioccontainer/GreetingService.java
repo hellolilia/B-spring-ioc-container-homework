15bd81dcc514539b593227d4e9ec8a07f02eb6d0
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-11-12 19:05:00 +0800

    completed p2s1

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,9 +1,16 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class GreetingService {
+
+    public GreetingService() {
+        System.out.println("Singleton instance created");
+    }
 
     String sayHi() {
         return "hello world";
